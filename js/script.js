836c03681c1d804f5585044995a82a1d66fbab93
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-26 16:52:12 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Solicitar ao usuário que ele insira **dois números** e, 
+ * a partir daí, calcular:
+
+- [ ]  A soma dos dois números;
+- [ ]  A subtração dos dois números;
+- [ ]  A multiplicação dos dois números;
+- [ ]  A divisão dos dois números;
+- [ ]  O resto da divisão dos dois números;
+
+ Para se desafiar:
+- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
+- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
+
+As operações matemáticas devem ser mostrada na tela.
+ */
+
+
